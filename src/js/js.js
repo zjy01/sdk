d6afd0a4191be5ec427245e59470fa9690d4da2f
zjy01 <525315462@qq.com>
2016-03-13 23:09:37 +0800

    修改电咱

--- src/js/js.js
+++ src/js/js.js
@@ -3,12 +3,13 @@
  */
 $(function () {
     var msg = {
-        render:'',
+        commentTpl: '',
+        likeTpl: '',
         IFMax: false,
         fetching: false,
         loaded:5,
         init: function () {
-            this.render = '<div class="comment-box">' +
+            this.commentTpl = '<div class="comment-box">' +
                 '<table>' +
                 '<tr>' +
                 '<td class="user-img-td" valign="top">' +
@@ -30,7 +31,12 @@ $(function () {
                 '</tr>' +
                 '</table>' +
                 '</div>';
+            this.likeTpl = '<div class="like-person">' +
+                '<img src="{{img}}">' +
+                '</div>';
+
             this.bindEvent();
+            this.loadLike();
         },
         bindEvent: function () {
             var self = this;
@@ -42,18 +48,19 @@ $(function () {
                 var RETop= refresh.offset().top;
                 if(RETop - LFTop - wh < 0 ){
                     if(!self.fetching){
-                        self.loadMsg(self.start,self.rows);
+                        self.loadComment(self.start,self.rows);
                     }
                 }
             });
         },
-        loadMsg: function () {
+        loadComment: function () {
             var self = this;
             self.fetching = true;
             $.ajax({
                 url: 'json/comment.json',
                 success: function (data) {
                     self.loaded += 5;
+                    var renderMsg = '';
                     data.msg.forEach(function (value) {
                         var view = {
                             img: value.user['user-img'],
@@ -61,16 +68,53 @@ $(function () {
                             content: value.comment.content,
                             time: value.comment.time
                         };
-                        var renderMsg = Mustache.render(self.render,view);
-                        $('.comment-area').append($(renderMsg));
+                        renderMsg += Mustache.render(self.commentTpl,view);
                     });
+
+                    $('.comment-area').append($(renderMsg));
                     if(self.loaded >= data.all){
                         $(window).off("scroll");
                         $("#comment-refresh").text("无更多评论");
                     }
                 },
                 complete: function () {
-                    console.log('com');
+                    self.fetching = false;
+                }
+            });
+        },
+        loadLike: function () {
+            var self = this;
+            var lw=$(".like-person-box").width();
+            var iw=56;
+            var num = Math.floor(lw/iw);
+            console.log(num);
+            $.ajax({
+                url: 'json/like.json',
+                data:{
+                  num:num
+                },
+                success: function (data) {
+                    var rn = 0;
+                    var renderMsg = '';
+                    var renderDot = '';
+                    if(data.all <= num ){
+                        rn = num;
+                    } else {
+                        rn = num - 1;
+                        renderDot = '<div class="like-person">' +
+                            '...' +
+                            '</div>';
+                    }
+                    for(var i = 0; i < rn; i++){
+                        var view = {
+                            img: data.data[i].img
+                        };
+                        renderMsg += Mustache.render(self.likeTpl,view);
+                    }
+                    renderMsg += renderDot;
+                    $('.like-person-box').append($(renderMsg));
+                },
+                complete: function () {
                     self.fetching = false;
                 }
             });
